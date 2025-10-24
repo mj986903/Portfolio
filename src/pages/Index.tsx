577c6c2ccde87ec1a90d1ea7c51aa64da2ad675b
Mohit Joshi <mj986903@gmail.com>
2025-10-24 16:17:10 +0530

    change

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,31 @@
 import { Navigation } from "@/components/Navigation";
 import { Hero } from "@/components/Hero";
+import { About } from "@/components/About";
+import { Experience } from "@/components/Experience";
+import { Projects } from "@/components/Projects";
+import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <Navigation resume="/Mohit.pdf" />
-      <div id="hero">
-        <Hero />
+      <Hero />
+      <div id="about">
+        <About />
       </div>
-      <Footer />
+      <div id="experience">
+        <Experience />
+      </div>
+      <div id="projects">
+        <Projects />
+      </div>
+      <div id="contact">
+        <Contact />
+      </div>
+      <div id="footer">
+        <Footer />
+      </div>
     </div>
   );
 };
